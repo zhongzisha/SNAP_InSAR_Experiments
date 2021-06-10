c48ba9c198f4caf5c2029d7485e92d24a72e2787
zhongzisha <zhongzisha@outlook.com>
2021-06-10 19:24:32 +0800

    seems OK

--- command3.bat
+++ command3.bat
@@ -1,5 +1,4 @@
 set SNAP_ROOT=E:\SNAP\snap-engine-9.0.0-SNAPSHOT
-set OUTPUT_DIR=%3
 set SNAP_MAIN_EXE=java -cp "%SNAP_ROOT%\modules\*;%SNAP_ROOT%\lib\*" -Dsnap.mainClass=org.esa.snap.core.gpf.main.GPT -Dsnap.home="%SNAP_ROOT%" -Djava.library.path="%SNAP_ROOT%\lib" -Xmx12G org.esa.snap.runtime.Launcher
 
 echo %SNAP_MAIN_EXE%
@@ -8,79 +7,84 @@ cd %SNAP_ROOT%
 
 E:
 
-if not exist "%OUTPUT_DIR%\step1.dim" (
-%SNAP_MAIN_EXE% ^
-G:\gdSAR\snap_exps\step1_gpt.xml ^
--Pinput1="G:\gdSAR\%1" ^
--Pinput2="G:\gdSAR\%2" ^
--Poutput1="%OUTPUT_DIR%\step1.dim"
-)
+REM set OUTPUT_DIR=%3
+
+REM if not exist "%OUTPUT_DIR%\step1.dim" (
+REM %SNAP_MAIN_EXE% ^
+REM G:\gdSAR\snap_exps\step1_gpt.xml ^
+REM -Pinput1="G:\gdSAR\%1" ^
+REM -Pinput2="G:\gdSAR\%2" ^
+REM -Poutput1="%OUTPUT_DIR%\step1.dim"
+REM )
 
 
-if not exist "%OUTPUT_DIR%\step2.dim" (
-%SNAP_MAIN_EXE% ^
-G:\gdSAR\snap_exps\step2_gpt.xml ^
--Pinput1="%OUTPUT_DIR%\step1.dim" ^
--Poutput1="%OUTPUT_DIR%\step2.dim"
-)
+REM if not exist "%OUTPUT_DIR%\step2.dim" (
+REM %SNAP_MAIN_EXE% ^
+REM G:\gdSAR\snap_exps\step2_gpt.xml ^
+REM -Pinput1="%OUTPUT_DIR%\step1.dim" ^
+REM -Poutput1="%OUTPUT_DIR%\step2.dim"
+REM )
 
-if not exist "%OUTPUT_DIR%\step3.dim" (
-%SNAP_MAIN_EXE% ^
-G:\gdSAR\snap_exps\step3_gpt.xml ^
--Pinput1="%OUTPUT_DIR%\step2.dim" ^
--Poutput1="%OUTPUT_DIR%\step3.dim"
-)
+REM if not exist "%OUTPUT_DIR%\step3.dim" (
+REM %SNAP_MAIN_EXE% ^
+REM G:\gdSAR\snap_exps\step3_gpt.xml ^
+REM -Pinput1="%OUTPUT_DIR%\step2.dim" ^
+REM -Poutput1="%OUTPUT_DIR%\step3.dim"
+REM )
 
-if not exist "%OUTPUT_DIR%\SNAPHU\snaphu.conf" (
-%SNAP_MAIN_EXE% ^
-G:\gdSAR\snap_exps\step4_gpt.xml ^
--Pinput1="%OUTPUT_DIR%\step3.dim" ^
--Poutput1="%OUTPUT_DIR%\SNAPHU"
-)
+REM if not exist "%OUTPUT_DIR%\SNAPHU\snaphu.conf" (
+REM %SNAP_MAIN_EXE% ^
+REM G:\gdSAR\snap_exps\step4_gpt.xml ^
+REM -Pinput1="%OUTPUT_DIR%\step3.dim" ^
+REM -Poutput1="%OUTPUT_DIR%\SNAPHU"
+REM )
 
-cd "%OUTPUT_DIR%\SNAPHU\step3"
+REM cd "%OUTPUT_DIR%\SNAPHU\step3"
 
-python G:\gdSAR\snap_exps\run_snaphu.py
+REM python G:\gdSAR\snap_exps\run_snaphu.py 0
 
-cd %SNAP_ROOT%
+REM cd %SNAP_ROOT%
 
-if not exist "%OUTPUT_DIR%\step6.dim" (
-%SNAP_MAIN_EXE% ^
-PhaseToDisplacement ^
--Ssource="%OUTPUT_DIR%\step5.dim" ^
--t "%OUTPUT_DIR%\step6.dim"
-)
+REM if not exist "%OUTPUT_DIR%\step6.dim" (
+REM %SNAP_MAIN_EXE% ^
+REM PhaseToDisplacement ^
+REM -Ssource="%OUTPUT_DIR%\step5.dim" ^
+REM -t "%OUTPUT_DIR%\step6.dim"
+REM )
 
-REM stacking
-if not exist "%OUTPUT_DIR%\step7.dim" (
-%SNAP_MAIN_EXE% ^
-G:\gdSAR\snap_exps\step6_gpt.xml ^
--Pinput1="%OUTPUT_DIR%\step6.dim,%OUTPUT_DIR%\step5.dim" ^
--Poutput1="%OUTPUT_DIR%\step7.dim"
-)
+REM REM stacking
+REM if not exist "%OUTPUT_DIR%\step7.dim" (
+REM %SNAP_MAIN_EXE% ^
+REM G:\gdSAR\snap_exps\step6_gpt.xml ^
+REM -Pinput1="%OUTPUT_DIR%\step6.dim,%OUTPUT_DIR%\step5.dim" ^
+REM -Poutput1="%OUTPUT_DIR%\step7.dim"
+REM )
 
-if not exist "%OUTPUT_DIR%\step8.dim" (
-%SNAP_MAIN_EXE% ^
-Terrain-Correction ^
--Ssource="%OUTPUT_DIR%\step7.dim" ^
--PsaveDEM=true ^
--t "%OUTPUT_DIR%\step8.dim"
-)
+REM if not exist "%OUTPUT_DIR%\step8.dim" (
+REM %SNAP_MAIN_EXE% ^
+REM Terrain-Correction ^
+REM -Ssource="%OUTPUT_DIR%\step7.dim" ^
+REM -PsaveDEM=true ^
+REM -t "%OUTPUT_DIR%\step8.dim"
+REM )
 
-if exist "%OUTPUT_DIR%\step7.dim" (
-rd /s /q "%OUTPUT_DIR%\step1.data"
-rd /s /q "%OUTPUT_DIR%\step2.data"
-rd /s /q "%OUTPUT_DIR%\step3.data"
-rd /s /q "%OUTPUT_DIR%\SNAPHU"
-rd /s /q "%OUTPUT_DIR%\step5.data"
-rd /s /q "%OUTPUT_DIR%\step6.data"
-rd /s /q "%OUTPUT_DIR%\step7.data"
-del %OUTPUT_DIR%\step1.dim
-del %OUTPUT_DIR%\step2.dim
-del %OUTPUT_DIR%\step3.dim
-del %OUTPUT_DIR%\step5.dim
-del %OUTPUT_DIR%\step6.dim
-del %OUTPUT_DIR%\step7.dim
-)
+REM if exist "%OUTPUT_DIR%\step7.dim" (
+REM rd /s /q "%OUTPUT_DIR%\step1.data"
+REM rd /s /q "%OUTPUT_DIR%\step2.data"
+REM rd /s /q "%OUTPUT_DIR%\step3.data"
+REM rd /s /q "%OUTPUT_DIR%\SNAPHU"
+REM rd /s /q "%OUTPUT_DIR%\step5.data"
+REM rd /s /q "%OUTPUT_DIR%\step6.data"
+REM rd /s /q "%OUTPUT_DIR%\step7.data"
+REM del %OUTPUT_DIR%\step1.dim
+REM del %OUTPUT_DIR%\step2.dim
+REM del %OUTPUT_DIR%\step3.dim
+REM del %OUTPUT_DIR%\step5.dim
+REM del %OUTPUT_DIR%\step6.dim
+REM del %OUTPUT_DIR%\step7.dim
+REM )
+
+set OUTPUT_DIR=%1
+python G:\gdSAR\snap_exps\run_snaphu.py 1 8 14
 
 
--- run_snaphu.py
+++ run_snaphu.py
@@ -1,4 +1,4 @@
-import os, glob
+import sys, os, glob
 print(os.environ['SNAP_ROOT'])
 SNAP_ROOT = os.environ['SNAP_ROOT']
 OUTPUT_DIR = os.environ['OUTPUT_DIR']
@@ -7,25 +7,39 @@ print(SNAP_ROOT)
 print(OUTPUT_DIR)
 print(SNAP_MAIN_EXE)
 
-filename = glob.glob('UnwPhase_ifg_*.snaphu.img')
-if len(filename) == 0:
-    with open('snaphu.conf', 'r') as fp:
-        lines = fp.readlines()
-        
-    cmd = None
-    for line in lines: 
-        if 'snaphu -f' in line:
-            cmd = line.replace('#', '')
-            break
+if sys.argv[1] == '0':
+
+    filename = glob.glob('UnwPhase_ifg_*.snaphu.img')
+    if len(filename) == 0:
+        with open('snaphu.conf', 'r') as fp:
+            lines = fp.readlines()
             
-    if cmd is not None:
+        cmd = None
+        for line in lines: 
+            if 'snaphu -f' in line:
+                cmd = line.replace('#', '')
+                break
+                
+        if cmd is not None:
+            print(cmd)
+            os.system(cmd)
+
+    filename = glob.glob('UnwPhase_ifg_*.snaphu.hdr')
+    if len(filename) > 0:
+        # os.system('cd %s' % os.environ['SNAP_ROOT'])
+        cmd = r'%s G:\gdSAR\snap_exps\step5_gpt.xml -Pinput1="%s\step3.dim" -Pinput2="%s\SNAPHU\step3\%s" -Poutput1="%s\step5.dim"' % (SNAP_MAIN_EXE, OUTPUT_DIR, OUTPUT_DIR, filename[0], OUTPUT_DIR)
         print(cmd)
         os.system(cmd)
 
-filename = glob.glob('UnwPhase_ifg_*.snaphu.hdr')
-if len(filename) > 0:
-    # os.system('cd %s' % os.environ['SNAP_ROOT'])
-    cmd = r'%s G:\gdSAR\snap_exps\step5_gpt.xml -Pinput1="%s\step3.dim" -Pinput2="%s\SNAPHU\step3\%s" -Poutput1="%s\step5.dim"' % (SNAP_MAIN_EXE, OUTPUT_DIR, OUTPUT_DIR, filename[0], OUTPUT_DIR)
+else:
+    OUTPUT_DIR_prefix = OUTPUT_DIR.split('_')[0]
+    start_id = int(sys.argv[2])
+    end_id = int(sys.argv[3])
+    save_filename = OUTPUT_DIR_prefix + r'_%d\Stack.dim'%(end_id+1)
+    dirs = []
+    for i in range(start_id, end_id+1):
+        dirs.append('%s_%d_%d\\step8.dim'%(OUTPUT_DIR_prefix, i, i+1))
+    input_filename = ','.join(dirs)
+    cmd = r'%s G:\gdSAR\snap_exps\step7_gpt.xml -Pinput1="%s" -Poutput1="%s"'%(SNAP_MAIN_EXE, input_filename, save_filename)
     print(cmd)
     os.system(cmd)
-
