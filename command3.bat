918bea7772074e3e3b7ee3a2ff54f874e032846c
zhongzisha <zhongzisha@outlook.com>
2021-07-04 22:20:47 +0800

    try batch for StaMPS

--- command3.bat
+++ command3.bat
@@ -7,66 +7,66 @@ cd %SNAP_ROOT%
 
 E:
 
-REM set OUTPUT_DIR=%3
+set OUTPUT_DIR=%3
 
-REM if not exist "%OUTPUT_DIR%\step1.dim" (
-REM %SNAP_MAIN_EXE% ^
-REM G:\gdSAR\snap_exps\step1_gpt.xml ^
-REM -Pinput1="G:\gdSAR\%1" ^
-REM -Pinput2="G:\gdSAR\%2" ^
-REM -Poutput1="%OUTPUT_DIR%\step1.dim"
-REM )
+if not exist "%OUTPUT_DIR%\step1.dim" (
+%SNAP_MAIN_EXE% ^
+G:\gdSAR\snap_exps\step1_gpt.xml ^
+-Pinput1="G:\gdSAR\%1" ^
+-Pinput2="G:\gdSAR\%2" ^
+-Poutput1="%OUTPUT_DIR%\step1.dim"
+)
 
 
-REM if not exist "%OUTPUT_DIR%\step2.dim" (
-REM %SNAP_MAIN_EXE% ^
-REM G:\gdSAR\snap_exps\step2_gpt.xml ^
-REM -Pinput1="%OUTPUT_DIR%\step1.dim" ^
-REM -Poutput1="%OUTPUT_DIR%\step2.dim"
-REM )
+if not exist "%OUTPUT_DIR%\step2.dim" (
+%SNAP_MAIN_EXE% ^
+G:\gdSAR\snap_exps\step2_gpt.xml ^
+-Pinput1="%OUTPUT_DIR%\step1.dim" ^
+-Poutput1="%OUTPUT_DIR%\step2.dim"
+)
 
-REM if not exist "%OUTPUT_DIR%\step3.dim" (
-REM %SNAP_MAIN_EXE% ^
-REM G:\gdSAR\snap_exps\step3_gpt.xml ^
-REM -Pinput1="%OUTPUT_DIR%\step2.dim" ^
-REM -Poutput1="%OUTPUT_DIR%\step3.dim"
-REM )
+if not exist "%OUTPUT_DIR%\step3.dim" (
+%SNAP_MAIN_EXE% ^
+G:\gdSAR\snap_exps\step3_gpt.xml ^
+-Pinput1="%OUTPUT_DIR%\step2.dim" ^
+-Poutput1="%OUTPUT_DIR%\step3.dim"
+)
 
-REM if not exist "%OUTPUT_DIR%\SNAPHU\snaphu.conf" (
-REM %SNAP_MAIN_EXE% ^
-REM G:\gdSAR\snap_exps\step4_gpt.xml ^
-REM -Pinput1="%OUTPUT_DIR%\step3.dim" ^
-REM -Poutput1="%OUTPUT_DIR%\SNAPHU"
-REM )
+if not exist "%OUTPUT_DIR%\SNAPHU\snaphu.conf" (
+%SNAP_MAIN_EXE% ^
+G:\gdSAR\snap_exps\step4_gpt.xml ^
+-Pinput1="%OUTPUT_DIR%\step3.dim" ^
+-Poutput1="%OUTPUT_DIR%\SNAPHU"
+)
 
-REM cd "%OUTPUT_DIR%\SNAPHU\step3"
+cd "%OUTPUT_DIR%\SNAPHU\step3"
 
-REM python G:\gdSAR\snap_exps\run_snaphu.py 0
+python G:\gdSAR\snap_exps\run_snaphu.py 0
 
-REM cd %SNAP_ROOT%
+cd %SNAP_ROOT%
 
-REM if not exist "%OUTPUT_DIR%\step6.dim" (
-REM %SNAP_MAIN_EXE% ^
-REM PhaseToDisplacement ^
-REM -Ssource="%OUTPUT_DIR%\step5.dim" ^
-REM -t "%OUTPUT_DIR%\step6.dim"
-REM )
+if not exist "%OUTPUT_DIR%\step6.dim" (
+%SNAP_MAIN_EXE% ^
+PhaseToDisplacement ^
+-Ssource="%OUTPUT_DIR%\step5.dim" ^
+-t "%OUTPUT_DIR%\step6.dim"
+)
 
-REM REM stacking
-REM if not exist "%OUTPUT_DIR%\step7.dim" (
-REM %SNAP_MAIN_EXE% ^
-REM G:\gdSAR\snap_exps\step6_gpt.xml ^
-REM -Pinput1="%OUTPUT_DIR%\step6.dim,%OUTPUT_DIR%\step5.dim" ^
-REM -Poutput1="%OUTPUT_DIR%\step7.dim"
-REM )
+REM stacking
+if not exist "%OUTPUT_DIR%\step7.dim" (
+%SNAP_MAIN_EXE% ^
+G:\gdSAR\snap_exps\step6_gpt.xml ^
+-Pinput1="%OUTPUT_DIR%\step6.dim,%OUTPUT_DIR%\step5.dim" ^
+-Poutput1="%OUTPUT_DIR%\step7.dim"
+)
 
-REM if not exist "%OUTPUT_DIR%\step8.dim" (
-REM %SNAP_MAIN_EXE% ^
-REM Terrain-Correction ^
-REM -Ssource="%OUTPUT_DIR%\step7.dim" ^
-REM -PsaveDEM=true ^
-REM -t "%OUTPUT_DIR%\step8.dim"
-REM )
+if not exist "%OUTPUT_DIR%\step8.dim" (
+%SNAP_MAIN_EXE% ^
+Terrain-Correction ^
+-Ssource="%OUTPUT_DIR%\step7.dim" ^
+-PsaveDEM=true ^
+-t "%OUTPUT_DIR%\step8.dim"
+)
 
 REM if exist "%OUTPUT_DIR%\step7.dim" (
 REM rd /s /q "%OUTPUT_DIR%\step1.data"
@@ -84,7 +84,7 @@ REM del %OUTPUT_DIR%\step6.dim
 REM del %OUTPUT_DIR%\step7.dim
 REM )
 
-set OUTPUT_DIR=%1
-python G:\gdSAR\snap_exps\run_snaphu.py 1 8 14
+REM set OUTPUT_DIR=%1
+REM python G:\gdSAR\snap_exps\run_snaphu.py 1 8 14
 
 
---
+++ step1_Split_and_ApplyOrbit.py
@@ -0,0 +1,63 @@
+
+
+import sys,os,glob
+import subprocess
+import time
+
+
+src_dir = r'G:\gdSAR'  # sys.argv[1]   # slaves
+dst_dir = r'E:\stamps\step1_results'  # sys.argv[2]
+
+if not os.path.exists(dst_dir):
+    os.makedirs(dst_dir)
+
+SNAP_ROOT = r'E:\SNAP\snap-engine-9.0.0-SNAPSHOT'
+SNAP_MAIN_EXE = r'java -cp "%s\modules\*;%s\lib\*" -Dsnap.mainClass=org.esa.snap.core.gpf.main.GPT -Dsnap.home="%s" ' \
+                r'-Djava.library.path="%s\lib" -Xmx12G org.esa.snap.runtime.Launcher' % \
+                (SNAP_ROOT, SNAP_ROOT, SNAP_ROOT, SNAP_ROOT)
+print(SNAP_MAIN_EXE)
+
+filenames = glob.glob(os.path.join(src_dir, 'S1A_*'))
+
+for k, filename in enumerate(filenames):
+    head, tail = os.path.split(filename)
+    print(tail[17:25])
+    command = r'%s step1_Split_and_ApplyOrbit.xml -Pinput1=%s -Poutput1=%s\%s.dim' % \
+              (SNAP_MAIN_EXE, filename, dst_dir, tail[17:25])
+    # launching the process
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    timeStarted = time.time()
+    stdout = process.communicate()[0]
+    print('SNAP STDOUT:{}'.format(stdout))
+    timeDelta = time.time() - timeStarted  # Get execution time.
+    print('[' + str(k) + '] Finished process in ' + str(timeDelta) + ' seconds.')
+    if process.returncode != 0:
+        message = 'Error splitting slave ' + str(filename)
+    else:
+        message = 'Split slave ' + str(filename) + ' successfully completed.\n'
+        print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
